28f59a232c61c77a0cb8e7423166064c375899af
Elias Chanquia <chanquiaelias8@gmail.com>
2023-07-06 12:49:42 -0300

    change

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const models_1 = __importDefault(require("../models"));
 const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+models_1.default.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`App listening port ${port}`);
+    });
+});
 app.listen(3000, () => {
     console.log('Server on port ', 3000);
 });
