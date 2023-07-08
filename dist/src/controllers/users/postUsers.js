d57879e3c0e03bf65f40e49489315affd884f23a
Elias Chanquia <chanquiaelias8@gmail.com>
2023-07-08 11:31:44 -0300

    dist update

---
+++ dist/src/controllers/users/postUsers.js
@@ -0,0 +1,44 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const User_1 = require("../../models/User");
+const createUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { userName, firstname, lastName, email, password, Birthdate, nationality, sex, type } = req.body;
+        const haduserName = yield User_1.User.count({ where: { userName } });
+        if (haduserName > 0)
+            return res.status(400).json({ error: "usuario ya existe" });
+        const hadmail = yield User_1.User.count({ where: { email } });
+        if (hadmail > 0)
+            return res.status(400).json({ error: "email ya existe" });
+        //formato de la fecha en el objeto JSON y utiliza el formato "YYYY-MM-DD". 
+        const birthdated = new Date(Birthdate);
+        const newUser = yield User_1.User.create({
+            userName,
+            firstname,
+            lastName,
+            email,
+            password,
+            Birthdate: birthdated,
+            nationality,
+            sex,
+            type
+        });
+        const newUserWithoutPassword = Object.assign(Object.assign({}, newUser.toJSON()), { password: undefined });
+        console.log(newUserWithoutPassword);
+        res.status(201).json(newUserWithoutPassword);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+exports.default = createUser;
--- dist/src/db.js
+++ dist/src/db.js
@@ -1,9 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
+exports.db = exports.User = exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 const config_1 = require("./config");
-exports.db = new sequelize_1.Sequelize(config_1.DB_NAME, config_1.DB_USER, config_1.DB_PASSWORD, {
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+exports.sequelize = new sequelize_1.Sequelize(config_1.DB_NAME, config_1.DB_USER, config_1.DB_PASSWORD, {
     host: config_1.DB_HOST,
     dialect: 'postgres'
 });
+exports.db = exports.sequelize;
+const basename = path_1.default.basename(__filename);
+const modelDefiners = [];
+fs_1.default.readdirSync(path_1.default.join(__dirname, '/models'))
+    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+    .forEach((file) => {
+    const model = require(path_1.default.join(__dirname, '/models', file));
+    modelDefiners.push(model.default);
+});
+modelDefiners.forEach((modelDefiner) => modelDefiner(exports.sequelize));
+let entries = Object.entries(exports.sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+const models = Object.fromEntries(capsEntries);
+const { User } = models;
+exports.User = User;
